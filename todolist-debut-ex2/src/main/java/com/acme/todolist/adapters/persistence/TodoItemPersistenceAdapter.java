affcd32ce84290ee0845204325bdf80bea86ab3a
E196470E <102021559+E196470E@users.noreply.github.com>
2024-02-23 11:04:55 +0100

    fin exerice 1

--- todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/persistence/TodoItemPersistenceAdapter.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/persistence/TodoItemPersistenceAdapter.java
@@ -5,11 +5,14 @@ import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import com.acme.todolist.application.port.out.LoadTodoItem;
 import com.acme.todolist.application.port.out.UpdateTodoItem;
 import com.acme.todolist.domain.TodoItem;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Implémentation JPA des port out de persistence
@@ -35,6 +38,13 @@ public class TodoItemPersistenceAdapter implements LoadTodoItem,UpdateTodoItem {
 	public List<TodoItem> loadAllTodoItems() {
 		return this.todoItemRepository.findAll().stream()
 				.map(todoItemJpaEntory -> mapper.mapToTodoItem(todoItemJpaEntory)).collect(Collectors.toList());
+	}
+
+	@Override
+	@PostMapping("/todos")
+	@ResponseStatus(code= HttpStatus.CREATED)
+	public void storeNewTodoItem(TodoItem item) {
+
 	}
 
 	// A compléter
